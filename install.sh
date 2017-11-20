ec91132b23aff94b884d612f0fb214939261f9d3
Guido Fioravantti <guido.fioravantti@gmail.com>
2017-11-20 13:28:03 +0100

    added overwrite options

--- install.sh
+++ install.sh
@@ -2,7 +2,22 @@
 
 for dotfile in $(find . -name ".[^.]*" -type f -maxdepth 1);
 do
-  echo "Linking $dotfile.."  
-  ln -s $dotfile ~/
+  echo "Processing $dotfile\c"
+
+  if [ -e $HOME/$dotfile ];
+  then
+    read -p " overwrite? (y/N) " -n 1 -r
+
+    if [[ ! $REPLY =~ ^[Yy]$ ]];
+    then
+      echo " ..skip"
+      continue
+    else
+      rm $HOME/$dotfile
+    fi;
+  fi;
+
+  echo " ..ok"
+  #ln -s $dotfile ~/
 done;
 
