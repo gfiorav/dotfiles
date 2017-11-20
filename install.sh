c6636bbc159202afa8c4b2435dec98f02503edf7
Guido Fioravantti <guido.fioravantti@gmail.com>
2017-11-20 13:36:55 +0100

    rely on ln -is instead of manual prompt

--- install.sh
+++ install.sh
@@ -2,22 +2,7 @@
 
 for dotfile in $(find . -name ".[^.]*" -type f -maxdepth 1);
 do
-  echo "Processing $dotfile\c"
-
-  if [ -e $HOME/$dotfile ];
-  then
-    read -p " overwrite? (y/N) " -n 1 -r
-
-    if [[ ! $REPLY =~ ^[Yy]$ ]];
-    then
-      echo " ..skip"
-      continue
-    else
-      rm $HOME/$dotfile
-    fi;
-  fi;
-
-  echo " ..ok"
-  ln -s $dotfile ~/
+  echo "Processing $dotfile"
+  ln -is $dotfile ~/
 done;
 
