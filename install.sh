85664491257a9f7542cec541f774b002d1689b02
Guido Fioravantti <guido.fioravantti@gmail.com>
2017-11-22 02:01:40 +0100

    Fix several issues with install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
 
 function prompt() {
-  read -p "$1 [y/N]" -n 1 -r
+  read -p "$1 [y/N]" -r
   echo
   if [[ $REPLY =~ ^[Yy]$ ]];
   then
-    return true;
+    return 0;
   else
-    return false;
+    return 1;
   fi
 }
 
-if [ prompt "Install and link brew's vim version for shared clipboard? (requires brew)" ];
+if $(prompt "Install and link brew's vim version for shared clipboard? (requires brew)");
 then
   if [ ! $(command -v brew) ];
   then
-    if [ prompt "brew is not installed, install it now?" ];
+    if $(prompt "brew is not installed, install it now?");
     then
+      echo "Installing brew..."
       /usr/bin/ruby \
         -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     fi
   fi
 
+  echo "Installing brew's vim..."
   brew install vim
   echo "alias vim=/usr/local/Cellar/vim/*/bin/vim" >> $HOME/.bashrc
   echo "alias vim=/usr/local/Cellar/vim/*/bin/vim" >> $HOME/.zshrc
@@ -29,7 +31,7 @@ fi
 
 if [ ! -d $HOME/.vim/bundle/Vundle.vim ];
 then
-  if [ prompt "Vundle is required for this vim setup, install it now?" ];
+  if $(prompt "Vundle is required for this vim setup, install it now?");
   then
     git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
   fi
@@ -37,7 +39,7 @@ fi
 
 for dotfile in src/*;
 do
-  echo "Processing $dotfilei..."
+  echo "Processing $dotfile..."
   ln -is $PWD/$dotfile $HOME/.$(basename $dotfile)
 done;
 
