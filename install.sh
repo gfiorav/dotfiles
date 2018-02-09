90739c26fc4adc74847160deb3ef2cd5bcdf13d0
Guido Fioravantti <guido.fioravantti@gmail.com>
2018-02-09 00:24:39 +0000

    Naive change to pacman

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ function prompt() {
 
 if $(prompt "Install git?");
 then
-  sudo apt install git libgnome-keyring-dev
+  sudo pacman -S git libgnome-keyring-dev
   sudo make --directory=/usr/share/doc/git/contrib/credential/gnome-keyring
 
   git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
@@ -31,37 +31,34 @@ fi
 
 if $(prompt "Install vim?");
 then
-  sudo apt install vim-gnome
+  sudo pacman -S vim-gnome
 fi
 
 if $(prompt "Install Google Chrome?");
 then
-  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-  sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-  sudo apt update
-  sudo apt install google-chrome-stable
+  sudo pacman -S google-chrome
 fi
 
 if $(prompt "Install fonts?");
 then
   curl -L https://github.com/hbin/top-programming-fonts/raw/master/install.sh | bash
-  sudo apt install ttf-mscorefonts-installer
+  sudo pacman -S ttf-mscorefonts-installer
   sudo fc-cache -f -v
 fi
 
 if $(prompt "Install tmux?");
 then
-  sudo apt install tmux 
+  sudo pacman -S tmux 
 fi
 
 if $(prompt "Install Arc themes?");
 then
-  sudo apt install arch-theme
+  sudo pacman -S arc-gtk-theme 
 fi
 
 if $(prompt "Install Gnome Tweak Tool?");
 then
-  sudo apt install gnome-tweak-tool
+  sudo pacman -S gnome-tweak-tool
 fi
 
 if $(prompt "Add GTK-3 modifications?");
@@ -71,7 +68,7 @@ fi
 
 if $(prompt "Install Laptop management stuff?");
 then
-  sudo apt install preload tlp thinkfan lm-sensors
+  sudo pacman -S preload tlp thinkfan lm-sensors
   sudo systemctl enable tlp
   sudo systemctl enable preload
 fi
