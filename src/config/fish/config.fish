953bb8066fafb4a819a96744fdd1d8cf7844f316
Guido Fioravantti <guido.fioravantti@gmail.com>
2020-08-19 17:45:08 -0400

    Update

--- src/config/fish/config.fish
+++ src/config/fish/config.fish
@@ -1,2 +1,2 @@
 set -x LIBGL_ALWAYS_INDIRECT 1
-set -x DISPLAY (awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null)
+set -x DISPLAY (awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
